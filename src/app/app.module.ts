7edff26da407f2e97cb394a1d00d767abdd64795
swathi <chamarthiswathi98@gmail.com>
2022-02-08 09:20:52 +0530

    login form done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegFormComponent } from './reg-form/reg-form.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeComponent } from './employee/employee.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegFormComponent,
-    LoginComponent
+    LoginComponent,
+    EmployeeComponent,
+    AddemployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private search: HttpClient, private route: Router) { }
+  loginForm = new FormGroup({
+    logemail: new FormControl("", [Validators.required, Validators.email]),
+    logpass: new FormControl("", [Validators.required])
+  })
+
+  get logemail() {
+    return this.loginForm.get('logemail')
+  }
+
+  get logpass() {
+    return this.loginForm.get('logpass')
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+    this.search.get(" http://localhost:3000/posts").subscribe(val => {
+      const use = val.find((a:any) => {
+        a.email == this.loginForm.value.logemail && a.password == this.loginForm.value.logpass
+      })
+      if (use) {
+        alert("login successfully")
+        this.loginForm.reset()
+        this.route.navigate(["employee"])
+      } else {
+        alert("something get wrong")
+      }
+    })
+
+  }
+
 
   ngOnInit(): void {
   }
 
 }
+
