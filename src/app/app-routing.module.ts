b9983c1b119805c9ced4bde8621eecc7429556ec
SunalRamesh <96284646+SunalRamesh@users.noreply.github.com>
2022-02-08 10:51:25 +0530

    path removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,7 @@ import {LoginComponent} from './login/login.component';
 import { RegFormComponent } from './reg-form/reg-form.component';
 import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [
-  {path:"register",component:RegFormComponent},
-  {path:"login",component:LoginComponent},
-  {path:"addEmployee",component:AddemployeeComponent},
-  {path:"employee",component:EmployeeComponent}
+const routes: Routes = [ 
 ];
 
 @NgModule({
